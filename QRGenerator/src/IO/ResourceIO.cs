d48d4a1fbd80efe4d21e21cd03eed099e100f868
lbroglio <lukebroglio@gmail.com>
2025-03-09 11:26:42 -0400

    Finished IO for encoding table

--- QRGenerator/src/IO/ResourceIO.cs
+++ QRGenerator/src/IO/ResourceIO.cs
@@ -1,10 +1,12 @@
+using System.Reflection;
+
 namespace QRGenerator.IO;
 
 /// <summary>
 /// Class holding methods to read in files included in the resources folder. Handles parsing the files into 
 /// specific formats.
 /// </summary>
-internal static class ResourceIO {
+public static class ResourceIO {
     /// <summary>
     /// Reads in a text file containing a collection of chars and the integer values they encode into <br/>
     /// The format of these files should be <br/>
@@ -13,8 +15,37 @@ internal static class ResourceIO {
     /// A 10 <br/>
     /// See Resources/QRAlphanumericEncodingTable.txt for an example
     /// </summary>
+    /// <param name="resourceName">The name of the file contaning the encoding tabled</param>
     /// <returns>A Dictionary with the chars read in as the key and the ints they encode to as the values</returns>
-    internal static Dictionary<char, int> ReadInEncodingTable(){
-        throw new NotImplementedException();
+    public static Dictionary<char, int> ReadInEncodingTable(string resourceName){
+        Dictionary<char, int> readDict = new Dictionary<char, int>();
+
+        //TODO: Verify the file format is correct
+        var assembly = Assembly.GetExecutingAssembly();
+        using (Stream? stream = assembly.GetManifestResourceStream(resourceName)){
+            if (stream == null){
+                throw new InvalidOperationException("Could not get resource stream for resource " + resourceName);
+            }
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                if(reader == null){
+                    throw new InvalidOperationException("Could not create stream reade.");
+                }
+                while(reader.Peek() != -1){
+                    char toEncode = (char) reader.Read();
+                    string? encodingStr = reader.ReadLine();
+
+                    if(encodingStr != null){
+                        int encoding = int.Parse(encodingStr);
+
+                        readDict.Add(toEncode, encoding);
+                    }                    
+
+                }
+            }
+
+        }
+
+        return readDict;
     }
 }
