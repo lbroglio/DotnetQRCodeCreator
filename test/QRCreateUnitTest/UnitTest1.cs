b17ff4d5e887f267182873f3e358ea16e85307d6
lbroglio <lukebroglio@gmail.com>
2025-08-11 17:38:03 -0500

    Restructured directories following decison not to use monorepo apporach. Added unit testing project

---
+++ test/QRCreateUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace QRCreateUnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
